2bf31fb91399837488e031cd2ba5292a7e7db60b
chichoc <kny6152@naver.com>
2024-11-05 15:46:19 +0900

    ✨ feat: vite-plugin-svgr 설치 및 import 확인

--- apps/client/src/pages/App.tsx
+++ apps/client/src/pages/App.tsx
@@ -1,5 +1,8 @@
+import TestSVG from '../../public/icons/code_copy.svg?react';
 function App() {
-  return <div className="bg-green-500">app</div>;
+  return <div className="bg-green-500">app 
+  <TestSVG/> 
+  </div>;
 }
 
 export default App;
