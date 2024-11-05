67fdfa128979e16931e287c5536c8d71843c2094
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2024-11-05 14:19:59 +0900

    ✨ feat: tailwindCss 환경 설정 추가 (#61)

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -3,36 +3,15 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      fontSize: {
-        XL: '1.5rem',
-        L: '1.25rem',
-        M: '1rem',
-        R: '0.875rem',
-        S: '0.75rem',
-      },
-      fontWeight: {
-        bold: '700',
-        semibold: '600',
-        medium: '500',
-      },
-      lineHeight: {
-        30: '1.875rem',
-        25: '1.5625rem',
-        24: '1.5rem',
-        21: '1.3125rem',
-        20: '1.25rem',
-        14: '0.875rem',
-        12: '0.75rem',
-      },
       colors: {
         gray: {
           white: '#FFFFFF',
           50: '#F4FBFA',
-          100: '#CDD6E4',
+          100: '#CDD9E4',
           200: '#90A2B7',
           300: '#768797',
           400: '#56687A',
-          500: '#415058',
+          500: '#41505B',
           black: '#1E272E',
         },
         yellow: {
@@ -41,12 +20,12 @@ export default {
         },
         green: {
           100: '#E5FBF3',
-          300: '#9DE0CE',
-          500: '#2D2085',
+          300: '#9DECCE',
+          500: '#02D085',
         },
         blue: {
           100: '#E2EDFF',
-          300: '#BBB5FF',
+          300: '#8BB5FF',
           500: '#3E84FF',
         },
         red: {
