cf19a6c85601398368e02319c93b8026db5b765a
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-05 17:22:22 +0900

    ✨ feat: workspace redoButton 추가

---
+++ apps/client/src/features/workspace/RedoButton.tsx
@@ -0,0 +1,11 @@
+import { CircleButton } from '@/shared/ui';
+import RightArrow from '@/shared/assets/arrow_right.svg?react';
+
+//TODO Redo 기능 추가
+export const RedoButton = () => {
+  return (
+    <CircleButton onClick={() => {}}>
+      <RightArrow />
+    </CircleButton>
+  );
+};
