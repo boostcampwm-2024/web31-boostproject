0ce610d2fd9c2821874dd93a74078a4905123a20
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-05 17:23:20 +0900

    🙀 chore: feature레이어 index.ts 추가 및 re-export 추가

---
+++ apps/client/src/features/index.ts
@@ -0,0 +1,4 @@
+export { UndoButton } from './workspace/UndoButton';
+export { RedoButton } from './workspace/RedoButton';
+export { SaveButton } from './workspace/SaveButton';
+export { WorkspaceNameInput } from './workspace/WorkspaceNameInput';
