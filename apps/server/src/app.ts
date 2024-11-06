6182a5e60b72ed18cacb4be800df0a01c767d1e4
Honghyeonji <hyeonji8436@gmail.com>
2024-11-07 02:03:21 +0900

    ✨ feat: mongoDB 연결 설정

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -2,8 +2,16 @@ import express from 'express';
 import routes from './routes/v1/index';
 import { swaggerUi } from './docs/swagger';
 import swaggerDocument from './docs/swagger-output.json';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
 const app = express();
+
+mongoose.connect(
+  process.env.MONGO_URI||""
+)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => { console.log(err) })
 
 // 미들웨어 설정
 app.use(express.json());
