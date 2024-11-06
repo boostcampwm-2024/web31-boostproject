86195d993e5046476c04e19c1d64967ce240dbc9
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-06 19:05:24 +0900

    ✨ feat: feat: Workspace Page 추가

---
+++ apps/client/src/pages/WorkspacePage.tsx
@@ -0,0 +1,10 @@
+import { WorkspaceHeader } from '@/widgets';
+
+//TODO useParams 훅을 통해 workspaceId 가져오기
+export const WorkspacePage = () => {
+  return (
+    <>
+      <WorkspaceHeader />
+    </>
+  );
+};
