b3757e0280a72ccf78c1c191b42176882171395c
leeyeongjae <xingxing2001@cau.ac.kr>
2024-11-07 15:31:15 +0900

    ✨ feat: 워크스페이스 이름 input 창 focus 해제 혹은 enter키 입력 시 토스트가 나오도록 기능 추가 (api 연동 x)

--- apps/client/src/entities/workspace/WorkspaceNameInput.tsx
+++ apps/client/src/entities/workspace/WorkspaceNameInput.tsx
@@ -1,13 +1,49 @@
+import React, { FocusEventHandler, KeyboardEventHandler, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
 export const WorkspaceNameInput = () => {
   // TODO: 워크스페이스 이름 변경 로직 필요
-  const handleBlur = () => {};
+
+  const [name, setName] = useState<string>('');
+
+  const [isNameChange, setIsNameChange] = useState<boolean>(false);
+  const handleBlur: FocusEventHandler<HTMLInputElement> = (
+    event: React.FocusEvent<HTMLInputElement>
+  ) => {
+    // TODO: 이름 변경 로직 추가
+    if (event.target.value === name) {
+      return;
+    }
+    toast.error('이름 변경 실패');
+    setName(event.target.value);
+  };
+
+  const handleEnter: KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key === 'Enter') {
+      handleKeyDown(e);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    // TODO: 이름 변경 로직 추가
+    e.currentTarget.blur();
+    if (name === e.currentTarget.value) {
+      return;
+    }
+    setName(e.currentTarget.value);
+    e.preventDefault();
+  };
 
   // TODO: 워크스페이스 이름 존재 시 placeholder가 그에 맞추어 변경되어야함
   return (
-    <input
-      placeholder="워크스페이스 이름"
-      className="placeholder:text-semibold-rg w-[272px] rounded-md border border-green-500 px-3 py-1 placeholder:text-gray-100 focus:outline-none"
-      onBlur={handleBlur}
-    />
+    <>
+      <Toaster position="bottom-center" reverseOrder={false} />
+      <input
+        placeholder={name === '' ? '워크스페이스 이름' : name}
+        className="placeholder:text-semibold-rg w-[272px] rounded-md border border-green-500 px-3 py-1 placeholder:text-gray-100 focus:outline-none"
+        onBlur={handleBlur}
+        onKeyDown={handleEnter}
+      />
+    </>
   );
 };
