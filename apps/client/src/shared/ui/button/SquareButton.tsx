03113dfbc9f702c5bd6a7c7c8f69eb1c0aa82a83
chichoc <kny6152@naver.com>
2024-11-07 14:31:11 +0900

    ✨ feat: SquareButton 컴포넌트 스타일 외부에서 조정

--- apps/client/src/shared/ui/button/SquareButton.tsx
+++ apps/client/src/shared/ui/button/SquareButton.tsx
@@ -1,15 +1,17 @@
 type SquareButtonProps = {
   children: string;
+  variant?: 'neutral' | 'danger';
   onClick: () => void;
 };
 
-export const SquareButton = ({ children, onClick }: SquareButtonProps) => {
-  // TODO: y/n버튼 구분
+export const SquareButton = ({ children, variant = 'neutral', onClick }: SquareButtonProps) => {
+  const colorClasses =
+    variant === 'danger'
+      ? 'bg-red-500 hover:bg-red-600 text-white'
+      : 'bg-gray-50 hover:bg-gray-100 text-gray-800';
+
   return (
-    <button
-      onClick={onClick}
-      className={`rounded-lg bg-red-500 px-[72px] py-4 text-white hover:bg-red-600`}
-    >
+    <button onClick={onClick} className={`rounded-lg px-[72px] py-4 ${colorClasses}`}>
       {children}
     </button>
   );
