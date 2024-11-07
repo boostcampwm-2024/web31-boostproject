6851eb805fdeef6b4ba88ed0ce887337fa37a149
leeyeongjae <xingxing2001@cau.ac.kr>
2024-11-07 13:43:07 +0900

    ✨ feat: HomeHeader에 isBlack Props 추가 => true : 검정배경 헤더, false : 흰 배경 헤더

--- apps/client/src/shared/ui/logo/Logo.tsx
+++ apps/client/src/shared/ui/logo/Logo.tsx
@@ -1,18 +1,19 @@
-import BlackLogoText from '@/shared/assets/boolock_logo_black.svg?react';
 import { Link } from 'react-router-dom';
+
+import BlackLogoText from '@/shared/assets/boolock_logo_black.svg?react';
 import LogoIcon from '@/shared/assets/boolock_icon.svg?react';
 import WhiteLogoText from '@/shared/assets/boolock_logo_white.svg?react';
 
 type LogoProps = {
-  isDark: boolean;
+  isBlack: boolean;
 };
 
-export const Logo = ({ isDark }: LogoProps) => {
+export const Logo = ({ isBlack }: LogoProps) => {
   return (
     <Link to="/">
       <div className="flex items-center gap-3">
         <LogoIcon className="h-8 w-8" />
-        {isDark ? <WhiteLogoText className="w-28" /> : <BlackLogoText className="w-28" />}
+        {isBlack ? <WhiteLogoText className="w-28" /> : <BlackLogoText className="w-28" />}
       </div>
     </Link>
   );
