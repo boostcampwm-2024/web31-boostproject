e14f0753fbde6081d559b953603780df1eeb66a7
leeyeongjae <xingxing2001@cau.ac.kr>
2024-11-07 13:39:51 +0900

    🔨 refactor: feature 레이어 삭제 및 entities 레이어로 이동

--- apps/client/src/features/index.ts
+++
@@ -1,6 +0,0 @@
-export { WorkspaceAddBtn } from './home/WorkspaceAddBtn';
-
-export { UndoButton } from './workspace/UndoButton';
-export { RedoButton } from './workspace/RedoButton';
-export { SaveButton } from './workspace/SaveButton';
-export { WorkspaceNameInput } from './workspace/WorkspaceNameInput';
