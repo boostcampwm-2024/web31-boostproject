4b981be13eb2fe5a3cdea6d3e421b1846876bbc2
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-12 01:25:25 +0900

    ✨ feat: workspace get 메서드 라우팅 추가

--- apps/server/src/routes/v1/workspaceRoute.ts
+++ apps/server/src/routes/v1/workspaceRoute.ts
@@ -6,3 +6,9 @@ export const workspaceRouter = express.Router();
 const workspaceController = WorkspaceController();
 
 workspaceRouter.post('/', workspaceController.createNewWorkspace);
+workspaceRouter.get('/', workspaceController.getWorkspaceListByPage);
+
+/**
+ * /workspace?pageNum=0
+ * header userId : userId
+ */
