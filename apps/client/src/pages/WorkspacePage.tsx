09fb290c65f29a5547b38ef2701575e0ec4390e9
inhachoi <80045891+inhachoi@users.noreply.github.com>
2024-11-11 20:59:13 +0900

    ✨ feat: Blockly 기본 틀 구현 (#84)

--- apps/client/src/pages/WorkspacePage.tsx
+++ apps/client/src/pages/WorkspacePage.tsx
@@ -1,10 +1,11 @@
-import { WorkspacePageHeader } from '@/widgets';
+import { WorkspacePageHeader, WorkspaceContent } from '@/widgets';
 
 // TODO: useParams 훅을 통해 workspaceId 가져오기
 export const WorkspacePage = () => {
   return (
     <>
       <WorkspacePageHeader />
+      <WorkspaceContent />
     </>
   );
 };
