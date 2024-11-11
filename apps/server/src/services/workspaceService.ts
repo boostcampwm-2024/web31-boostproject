5ad1f3fdbb0df9f9b7ebd2e2d7712742c485c56d
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-12 01:24:04 +0900

    ✨ feat: skip, limit 메서드를 통해 db에서 페이지네이션 구현 return 값 : 총 워크스페이스 개수, 현재 페이지, 현재 페이지의 workspace, 페이지 당 workspace 개수

--- apps/server/src/services/workspaceService.ts
+++ apps/server/src/services/workspaceService.ts
@@ -1,9 +1,9 @@
 import { Workspace } from '@/models/workspaceModel';
 
 export const WorkspaceService = () => {
-  const createWorkspace = async () => {
+  const createWorkspace = async (userId: string) => {
     const newWorkspaceId = crypto.randomUUID();
-    const workspace = new Workspace({ workspace_id: newWorkspaceId });
+    const workspace = new Workspace({ user_id: userId, workspace_id: newWorkspaceId });
     try {
       await workspace.save();
       return newWorkspaceId;
@@ -15,7 +15,34 @@ export const WorkspaceService = () => {
     }
   };
 
+  /**
+   * @description
+   * workspace 페이지네이션 get 메서드
+   * userId : header,  page : query string
+   * @param userId
+   * @param page
+   * @return
+   * totalWorkspaceCount : 해당 유저의 총 워크스페이스 개수
+   * currentPage : 현재 페이지
+   * workspacePerPage : 페이지 당 워크스페이스 수
+   */
+  const findWorkspaceListByPage = async (userId: string, page: number) => {
+    try {
+      const totalWorkspace = await Workspace.find({ user_id: userId });
+      const totalWorkspaceCount = totalWorkspace.length;
+      const workspaceListOfCurrentPage = await Workspace.find({ user_id: userId })
+        .skip(20 * page)
+        .limit(20);
+      return {
+        totalWorkspaceCount,
+        workspaceListOfCurrentPage,
+        currentPage: page,
+        workspacePerPage: 20,
+      };
+    } catch (error) {}
+  };
   return {
     createWorkspace,
+    findWorkspaceListByPage,
   };
 };
