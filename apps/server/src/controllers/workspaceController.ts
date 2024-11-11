1b25d2101864e7789c85181fb4969640d8ddf488
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-11 22:18:04 +0900

    💬 comment: swagger jsDoc 추가

--- apps/server/src/controllers/workspaceController.ts
+++ apps/server/src/controllers/workspaceController.ts
@@ -6,6 +6,31 @@ import { WorkspaceService } from '@/services/workspaceService';
 export const WorkspaceController = () => {
   const workspaceService = WorkspaceService();
   const createNewWorkspace = async (req: Request, res: Response) => {
+    /* 
+  #swagger.summary = '새로운 워크스페이스 생성'
+  #swagger.description = '새로운 워크스페이스를 생성합니다.'
+  #swagger.tags = ['Workspace']
+  #swagger.responses[201] = {
+    description: 'success',
+    content: {
+      "application/json": {
+        schema: {
+          type: "object",
+          properties: {
+            newWorkspaceId: {
+              type: "string",
+              example: "b15eac31-3942-4192-9cbd-2e2cdd48da0a"
+            }
+          }
+        }
+      }
+    }
+  }
+  #swagger.responses[500] = {
+    description: 'internal server error'
+  }
+*/
+
     try {
       const newWorkspaceId = await workspaceService.createWorkspace();
       res.status(201).json({ newWorkspaceId });
