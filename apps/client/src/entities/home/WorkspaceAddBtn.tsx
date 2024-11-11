a5ef693f97c4a4bed91506f6bbff70d0bfa69058
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-11 21:38:49 +0900

    ✨ feat: api 요청 대기 시 add 버튼 disable로 변경

--- apps/client/src/entities/home/WorkspaceAddBtn.tsx
+++ apps/client/src/entities/home/WorkspaceAddBtn.tsx
@@ -1,16 +1,18 @@
 import { CircleButton } from '@/shared/ui';
 import PlusSVG from '@/shared/assets/plus.svg?react';
 import { useCreateWorkspace } from '@/shared/hooks';
+import { useLoadingStore } from '@/shared/store';
 
 export const WorkspaceAddBtn = () => {
   const { mutate: createWorkspace } = useCreateWorkspace();
+  const { isPending } = useLoadingStore();
 
   const handleClick = () => {
     createWorkspace();
   };
 
   return (
-    <CircleButton width="w-5" height="w-5" onClick={handleClick}>
+    <CircleButton width="w-5" height="w-5" onClick={handleClick} disable={isPending}>
       <PlusSVG width="12" height="12" />
     </CircleButton>
   );
