6dc590df53632558728c8b8e6d7e0d899142fbca
leeyeongjae <xingxing2001@cau.ac.kr>
2024-11-12 18:55:37 +0900

    ✨ feat: 무한스크롤 기능 적용

--- apps/client/src/widgets/home/WorkspaceContainer.tsx
+++ apps/client/src/widgets/home/WorkspaceContainer.tsx
@@ -2,9 +2,41 @@ import { useGetWorkspaceList } from '@/shared/hooks';
 import { WorkspaceHeader } from './WorkspaceHeader';
 import { WorkspaceList } from './WorkspaceList';
 import { EmptyWorkspace } from './EmptyWorkspace';
+import { useEffect, useRef } from 'react';
 
 export const WorkspaceContainer = () => {
   const { hasNextPage, fetchNextPage, isFetchingNextPage, workspaceList } = useGetWorkspaceList();
+
+  const nextFetchTargetRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5,
+    };
+
+    const fetchCallback: IntersectionObserverCallback = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && hasNextPage) {
+          fetchNextPage();
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(fetchCallback, options);
+
+    if (nextFetchTargetRef.current) {
+      observer.observe(nextFetchTargetRef.current);
+    }
+
+    return () => {
+      if (nextFetchTargetRef.current) {
+        observer.unobserve(nextFetchTargetRef.current);
+      }
+    };
+  }, [workspaceList]);
 
   return (
     <section className="pb-48">
@@ -14,7 +46,7 @@ export const WorkspaceContainer = () => {
       ) : (
         <>
           <WorkspaceList workspaceList={workspaceList} />
-          <div className="h-3 w-full"></div>
+          <div ref={nextFetchTargetRef} className="h-3 w-full"></div>
         </>
       )}
     </section>
