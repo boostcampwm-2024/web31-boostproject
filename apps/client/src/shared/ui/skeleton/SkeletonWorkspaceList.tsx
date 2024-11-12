660c3edbd50e2a7463e1e3d24e2c78cc2ecf8177
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-13 00:59:16 +0900

    🙀 chore: 디버깅용 코드 삭제

--- apps/client/src/shared/ui/skeleton/SkeletonWorkspaceList.tsx
+++ apps/client/src/shared/ui/skeleton/SkeletonWorkspaceList.tsx
@@ -4,7 +4,6 @@ export const SkeletonWorkspaceList = ({ skeletonNum }: { skeletonNum: number }) 
   return (
     <>
       {new Array(skeletonNum).fill(0).map((_, idx) => {
-        console.log(idx);
         return <SkeletonWorkspace key={idx} />;
       })}
     </>
