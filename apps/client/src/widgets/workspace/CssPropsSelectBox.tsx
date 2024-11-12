a26021a100673dd92ebd658386a612675fcd5534
inhachoi <80045891+inhachoi@users.noreply.github.com>
2024-11-12 23:53:53 +0900

    🔨 refactor: 시맨틱 태그 사용 + 변수명 직관적 개선

--- apps/client/src/widgets/workspace/CssPropsSelectBox.tsx
+++ apps/client/src/widgets/workspace/CssPropsSelectBox.tsx
@@ -37,7 +37,7 @@ export const CssPropsSelectBox = () => {
     },
   };
 
-  const [selectedCategory, setSelectedCategory] = useState<keyof typeof cssCategoriesObj>('layout');
+  const [selectedCssCategory, setSelectedCssCategory] = useState('layout');
   const [checkedProperties, setCheckedProperties] = useState<{ [key: string]: boolean }>({});
 
   // 체크박스 상태 변경 함수
@@ -49,38 +49,40 @@ export const CssPropsSelectBox = () => {
   };
 
   return (
-    <div className="flex h-[392px] w-[504px]">
-      <div className="border-r border-r-gray-100 p-2">
+    <section className="flex h-[392px] w-[504px]">
+      <nav className="border-r border-r-gray-100 p-2">
         {Object.keys(cssCategoriesObj).map((cssCategory) => (
-          <div
+          <button
             key={cssCategory}
-            onClick={() => setSelectedCategory(cssCategory)}
-            className={`text-bold-sm flex h-[36px] w-[100px] cursor-pointer items-center rounded p-2 text-gray-200 ${selectedCategory === cssCategory && 'text-gray-black bg-yellow-500'}`}
+            onClick={() => setSelectedCssCategory(cssCategory)}
+            className={`mb-1.5 text-bold-sm flex h-[36px] w-[100px] cursor-pointer items-center rounded p-2 text-gray-200 ${selectedCssCategory === cssCategory && 'text-gray-black bg-yellow-500'}`}
           >
             {cssCategory}
-          </div>
+          </button>
         ))}
-      </div>
-      <div className="space-y-2 overflow-hidden p-3">
-        {cssCategoriesObj[selectedCategory].items.map((property, index) => (
+      </nav>
+      <article className="space-y-2 overflow-y-auto p-3">
+        {cssCategoriesObj[selectedCssCategory].items.map((cssName, index) => (
           <div
             key={index}
             className={`flex h-[66px] w-[344px] items-center rounded-lg px-4 ${
-              checkedProperties[property] ? 'bg-yellow-500' : 'bg-gray-50'
+              checkedProperties[cssName] ? 'bg-yellow-500' : 'bg-gray-50'
             }`}
           >
             <input
               type="checkbox"
-              checked={!!checkedProperties[property]}
-              onChange={() => handleCheckboxChange(property)}
-              title={property}
+              checked={!!checkedProperties[cssName]}
+              onChange={() => handleCheckboxChange(cssName)}
+              title={cssName}
               className="h-5 w-5 appearance-none rounded border border-gray-100 bg-center bg-no-repeat checked:bg-white checked:bg-[url('@/shared/assets/check.svg')]"
             />
-            <p className="text-semibold-md text-gray-black border-gray-100 pl-5 pr-2">{property}</p>
+            <span className="text-semibold-md text-gray-black border-gray-100 pl-5 pr-2">
+              {cssName}
+            </span>
             <Question />
           </div>
         ))}
-      </div>
-    </div>
+      </article>
+    </section>
   );
 };
