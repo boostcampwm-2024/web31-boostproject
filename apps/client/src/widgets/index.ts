944b6ae7dd05fbb49d6c3f85b4da374fe85c9a39
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-13 01:02:10 +0900

    🙀 chore: export 순서 설정

--- apps/client/src/widgets/index.ts
+++ apps/client/src/widgets/index.ts
@@ -3,8 +3,9 @@ export { HomeHeader } from './home/HomeHeader';
 export { WorkspaceList } from './home/WorkspaceList';
 export { WorkspaceHeader } from './home/WorkspaceHeader';
 export { EmptyWorkspace } from './home/EmptyWorkspace';
-export { WorkspacePageHeader } from './workspace/WorkspacePageHeader';
-
+export { WorkspaceGrid } from './home/WorkspaceGrid';
 export { WorkspaceContainer } from './home/WorkspaceContainer';
 export { WorkspaceModal } from './home/WorkspaceModal';
+
 export { WorkspaceContent } from './workspace/WorkspaceContent';
+export { WorkspacePageHeader } from './workspace/WorkspacePageHeader';
