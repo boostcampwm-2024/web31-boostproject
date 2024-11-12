14c384c32e59a28f8f7e1fa9dd38ededf786cbbb
leeyeongjae <xingxing2001@cau.ac.kr>
2024-11-12 15:14:11 +0900

    💬 comment: api 요청 메소드 JsDoc 추가

--- apps/server/src/controllers/workspaceController.ts
+++ apps/server/src/controllers/workspaceController.ts
@@ -5,9 +5,17 @@ import { WorkspaceService } from '@/services/workspaceService';
 
 export const WorkspaceController = () => {
   const workspaceService = WorkspaceService();
+
+  /**
+   * @description
+   * 워크스페이스 생성 요청 api입니다.
+   * @method POST
+   * @headers { user-id : userId }
+   * @response newWorkspaceId (생성된 workspaceId)
+   */
   const createNewWorkspace = async (req: Request, res: Response) => {
     try {
-      const { userId } = req.body;
+      const userId = req.get('user-id') as string;
       const newWorkspaceId = await workspaceService.createWorkspace(userId);
       res.status(201).json({ newWorkspaceId });
     } catch (error) {
@@ -17,21 +25,27 @@ export const WorkspaceController = () => {
   };
 
   /**
-   * /workspace?pageNum=0
-   * header userId : userId
+   * @desciption
+   * 워크스페이스 조회 api입니다.
+   * 커서 기반 페이지네이션을 적용해서 20개씩 보냅니다.
+   * @method GET
+   * @headers { user-id : userId}
+   * @queryString cursor : {updatedAt, workspaceId}
+   * @response
+   * workspaceList : 현재 페이지의 워크스페이스
+   * nextCursor : 다음 페이지네이션 시작 커서
    */
-
   const getWorkspaceListByPage = async (req: Request, res: Response) => {
     try {
-      const userId = req.get('userid');
-      const page = req.query.page;
-      const response = await workspaceService.findWorkspaceListByPage(
-        userId as string,
-        Number(page)
+      const userId = req.get('user-id') as string;
+      const cursor = req.query.cursor ? JSON.parse(req.query.cursor as string) : null;
+      const pagedWorkspaceListResult = await workspaceService.findWorkspaceListByPage(
+        userId,
+        cursor
       );
-
-      res.status(200).json({ response });
+      res.status(200).json({ pagedWorkspaceListResult });
     } catch (error) {
+      console.error(error);
       res.status(500).json({ message: ERROR_MESSAGE[500] });
     }
   };
