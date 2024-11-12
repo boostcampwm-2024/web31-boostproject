998c82bd05cc65ca76dd32f60db48a8ae8ef268c
leeyeongjae <xingxing2001@cau.ac.kr>
2024-11-12 18:41:54 +0900

    🙀 chore: 디버깅용 코드 삭제

--- apps/client/src/widgets/home/WorkspaceList.tsx
+++ apps/client/src/widgets/home/WorkspaceList.tsx
@@ -9,7 +9,6 @@ export const WorkspaceList = ({ workspaceList }: workspaceListProps) => {
   return (
     <ul className="grid-cols-list grid w-[1128px] justify-start gap-x-6 gap-y-8">
       {workspaceList.map((workspace) => {
-        console.log(workspace.updated_at);
         return (
           <WorkspaceItem
             key={workspace.workspace_id}
