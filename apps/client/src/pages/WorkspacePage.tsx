b0accfd3ae1e5b77b8336f119b5683f93c0964f4
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-13 23:47:55 +0900

    ✨ feat: 단일 워크스페이스 정보 조회 api 연동

--- apps/client/src/pages/WorkspacePage.tsx
+++ apps/client/src/pages/WorkspacePage.tsx
@@ -1,10 +1,28 @@
-import { WorkspacePageHeader, WorkspaceContent } from '@/widgets';
+import { WorkspaceContent, WorkspacePageHeader } from '@/widgets';
+
+import { Loading } from '@/shared/ui';
+import { useEffect } from 'react';
+import { useGetWorkspace } from '@/shared/hooks';
+import { useParams } from 'react-router-dom';
 
 // TODO: useParams 훅을 통해 workspaceId 가져오기
 export const WorkspacePage = () => {
+  const params = useParams();
+  const { workspaceId } = params;
+
+  const { data, isPending, isError } = useGetWorkspace(workspaceId as string);
+  useEffect(() => {
+    console.log(data);
+  }, [isPending]);
+
+  if (isError) {
+    return <div>에러가 발생했습니다.</div>;
+  }
+
   return (
     <>
-      <WorkspacePageHeader />
+      {isPending && <Loading />}
+      {data && !isError && <WorkspacePageHeader workspace={data} />}
       <WorkspaceContent />
     </>
   );
