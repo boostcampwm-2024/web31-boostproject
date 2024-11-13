93a97a0b5fde411ce315c6c8f9b532c5ba42819d
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-14 01:33:21 +0900

    ✨ feat: 워크스페이스 정보를 불러오는 것을 실패하면 NotFound 페이지를 렌더링함

--- apps/client/src/pages/WorkspacePage.tsx
+++ apps/client/src/pages/WorkspacePage.tsx
@@ -1,28 +1,23 @@
 import { WorkspaceContent, WorkspacePageHeader } from '@/widgets';
 
 import { Loading } from '@/shared/ui';
-import { useEffect } from 'react';
+import { NotFound } from '@/pages/NotFound';
 import { useGetWorkspace } from '@/shared/hooks';
 import { useParams } from 'react-router-dom';
 
-// TODO: useParams 훅을 통해 workspaceId 가져오기
 export const WorkspacePage = () => {
-  const params = useParams();
-  const { workspaceId } = params;
+  const { workspaceId } = useParams();
 
-  const { data, isPending, isError } = useGetWorkspace(workspaceId as string);
-  useEffect(() => {
-    console.log(data);
-  }, [isPending]);
+  const { isPending, isError } = useGetWorkspace(workspaceId as string);
 
   if (isError) {
-    return <div>에러가 발생했습니다.</div>;
+    return <NotFound />;
   }
 
   return (
     <>
       {isPending && <Loading />}
-      {data && !isError && <WorkspacePageHeader workspace={data} />}
+      <WorkspacePageHeader />
       <WorkspaceContent />
     </>
   );
