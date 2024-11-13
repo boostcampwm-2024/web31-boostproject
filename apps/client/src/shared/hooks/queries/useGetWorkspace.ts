64dcae19638db99fc813d307aaeba0315a3b5767
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-14 01:32:25 +0900

    ✨ feat: useGetWorkspace 커스텀 훅으로 workspace의 정보를 요청할 시 성공하면 workspace 의 name을 전역 상태로 관리, 실패하면 toast메세지 렌더링

--- apps/client/src/shared/hooks/queries/useGetWorkspace.ts
+++ apps/client/src/shared/hooks/queries/useGetWorkspace.ts
@@ -1,17 +1,30 @@
+import React from 'react';
 import { WorkspaceApi } from '@/shared/api';
 import { getUserId } from '@/shared/utils';
+import toast from 'react-hot-toast';
 import { useQuery } from '@tanstack/react-query';
+import { useWorkspaceStore } from '@/shared/store';
 
 export const useGetWorkspace = (workspaceId: string) => {
   const workspaceApi = WorkspaceApi();
   const userId = getUserId();
-
+  const { setName } = useWorkspaceStore();
   const { data, isPending, isError } = useQuery({
     queryKey: ['getWorkspace', workspaceId],
     queryFn: () => {
       return workspaceApi.getWorkspace(userId, workspaceId);
     },
   });
+
+  React.useEffect(() => {
+    if (isError) {
+      toast.error('워크스페이스 정보 불러오기 실패');
+      return;
+    }
+    if (data) {
+      setName(data.workspaceDto.name);
+    }
+  }, [data, setName, isError]);
 
   return { data, isPending, isError };
 };
