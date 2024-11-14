e82cb6f06f6e7c8a52f3a4d5518d2ad06cd5e4e5
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-15 00:03:59 +0900

    ✨ feat: delete 요청 시 모달의 로딩 상태를 설정하는 기능 추가

--- apps/client/src/shared/hooks/queries/useDeleteWorkspace.ts
+++ apps/client/src/shared/hooks/queries/useDeleteWorkspace.ts
@@ -1,16 +1,18 @@
-import { WorkspaceApi } from '@/shared/api';
-import { useModalStore } from '@/shared/store';
-import { getUserId } from '@/shared/utils';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { WorkspaceApi } from '@/shared/api';
+import { getUserId } from '@/shared/utils';
 import toast from 'react-hot-toast';
+import { useModalStore } from '@/shared/store';
 
 export const useDeleteWorkspace = () => {
   const queryClient = useQueryClient();
   const workspaceApi = WorkspaceApi();
   const userId = getUserId();
-  const { closeModal } = useModalStore();
+  const { closeModal, setIsLoading } = useModalStore();
   const { mutate, isPending, isError } = useMutation({
     mutationFn: (workspaceId: string) => {
+      setIsLoading(true);
       return workspaceApi.deleteWorkspace(userId, workspaceId);
     },
     onSuccess: () => {
@@ -21,6 +23,7 @@ export const useDeleteWorkspace = () => {
       toast.error('워크스페이스 삭제 실패');
     },
     onSettled: () => {
+      setIsLoading(false);
       closeModal();
     },
   });
