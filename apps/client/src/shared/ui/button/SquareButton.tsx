07dd6517d07de9e58a21877ea5851613785e75da
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-15 00:04:33 +0900

    ✨ feat: 버튼의 비활성화 여부를 가리키는 isDisabled 상태 변수 추가

--- apps/client/src/shared/ui/button/SquareButton.tsx
+++ apps/client/src/shared/ui/button/SquareButton.tsx
@@ -2,16 +2,25 @@ type SquareButtonProps = {
   children: string;
   variant?: 'neutral' | 'danger';
   onClick: () => void;
+  isDisabled?: boolean;
 };
 
-export const SquareButton = ({ children, variant = 'neutral', onClick }: SquareButtonProps) => {
+export const SquareButton = ({
+  children,
+  variant = 'neutral',
+  onClick,
+  isDisabled,
+}: SquareButtonProps) => {
   const colorClasses =
     variant === 'danger'
       ? 'bg-red-500 hover:bg-red-600 text-white'
       : 'bg-gray-50 hover:bg-gray-100 text-gray-300';
-
   return (
-    <button onClick={onClick} className={`text-bold-md rounded-lg px-[72px] py-4 ${colorClasses}`}>
+    <button
+      onClick={onClick}
+      className={`text-bold-md rounded-lg px-[72px] py-4 ${colorClasses}`}
+      disabled={isDisabled && true}
+    >
       {children}
     </button>
   );
