cc2fb9cae49b6434a9289225a5128a8bb2149c4d
leeyeongjae <xingxing2001@cau.ac.kr>
2024-11-14 21:10:19 +0900

    ✨ feat: useMutation으로 workspace의 삭제 요청을 처리하는 커스텀 훅 추가

--- apps/client/src/shared/hooks/index.ts
+++ apps/client/src/shared/hooks/index.ts
@@ -2,3 +2,4 @@ export { useCreateWorkspace } from './queries/useCreateWorkspace';
 export { useGetWorkspaceList } from './queries/useGetWorkspaceList';
 export { useGetWorkspace } from './queries/useGetWorkspace';
 export { useUpdateWorkspaceName } from './queries/useUpdateWorkspaceName';
+export { useDeleteWorkspace } from './queries/useDeleteWorkspace';
