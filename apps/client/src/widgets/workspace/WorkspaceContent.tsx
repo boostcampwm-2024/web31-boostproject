2195f272fb7315f3d3aed5b4d2c81ed03f29b675
inhachoi <80045891+inhachoi@users.noreply.github.com>
2024-11-18 20:58:01 +0900

    🙀 chore: 함수 위치 변경

--- apps/client/src/widgets/workspace/WorkspaceContent.tsx
+++ apps/client/src/widgets/workspace/WorkspaceContent.tsx
@@ -149,44 +149,6 @@ const contents = [
   },
 ];
 
-// prompt를 이용한 동적 생성
-const openTypedVariableModal = (workspace: Blockly.WorkspaceSvg) => {
-  const blockName = prompt('새로운 css 블록 이름을 입력하세요.');
-
-  if (blockName?.trim() === '') {
-    return alert('블록 이름을 입력해주세요.');
-  }
-
-  if (!Blockly.Blocks[blockName!]) {
-    Blockly.Blocks[blockName!] = {
-      init: function () {
-        this.appendDummyInput().appendField(new Blockly.FieldTextInput(blockName!), 'CLASS'); // 입력된 이름 반영
-        this.setOutput(true);
-        this.setColour(230);
-      },
-    };
-  }
-
-  // "폼" 카테고리를 찾아 기존 블록 유지 및 새 블록 추가
-  const formCategory = toolboxConfig.contents.find((category) => category.name === '폼');
-
-  // 기존 블록 유지 및 새 블록 추가
-  const existingBlocks = formCategory!.contents || [];
-  const isBlockAlreadyAdded = existingBlocks.some((block) => block.type === blockName);
-
-  if (isBlockAlreadyAdded) {
-    alert(`"${blockName}" 블록은 이미 "폼" 카테고리에 존재합니다.`);
-    return;
-  }
-
-  formCategory!.contents = [...existingBlocks, { kind: 'block', type: blockName }];
-
-  // 기존 툴박스 갱신
-  workspace.updateToolbox(toolboxConfig);
-
-  alert(`새 블록 "${blockName}"이(가) "폼" 카테고리에 성공적으로 추가되었습니다.`);
-};
-
 const toolboxConfig = {
   kind: 'categoryToolbox',
   contents: [
@@ -251,14 +213,6 @@ export const WorkspaceContent = () => {
   const [workspace, setWorkspace] = useState<Blockly.WorkspaceSvg | null>(null);
   const [htmlCode, setHtmlCode] = useState<string>('');
   const [activeTab, setActiveTab] = useState<'preview' | 'html' | 'css'>('preview');
-
-  const generateHtmlCode = () => {
-    if (!workspace) {
-      return;
-    }
-    const code = htmlCodeGenerator.workspaceToCode(workspace);
-    setHtmlCode(code);
-  };
 
   useEffect(() => {
     const newWorkspace = Blockly.inject('blocklyDiv', {
@@ -330,6 +284,52 @@ export const WorkspaceContent = () => {
       newWorkspace.dispose();
     };
   }, []);
+
+  // prompt를 이용한 동적 생성
+  const openTypedVariableModal = (workspace: Blockly.WorkspaceSvg) => {
+    const blockName = prompt('새로운 css 블록 이름을 입력하세요.');
+
+    if (blockName?.trim() === '') {
+      return alert('블록 이름을 입력해주세요.');
+    }
+
+    if (!Blockly.Blocks[blockName!]) {
+      Blockly.Blocks[blockName!] = {
+        init: function () {
+          this.appendDummyInput().appendField(new Blockly.FieldTextInput(blockName!), 'CLASS'); // 입력된 이름 반영
+          this.setOutput(true);
+          this.setColour(230);
+        },
+      };
+    }
+
+    // "폼" 카테고리를 찾아 기존 블록 유지 및 새 블록 추가
+    const formCategory = toolboxConfig.contents.find((category) => category.name === '폼');
+
+    // 기존 블록 유지 및 새 블록 추가
+    const existingBlocks = formCategory!.contents || [];
+    const isBlockAlreadyAdded = existingBlocks.some((block) => block.type === blockName);
+
+    if (isBlockAlreadyAdded) {
+      alert(`"${blockName}" 블록은 이미 "폼" 카테고리에 존재합니다.`);
+      return;
+    }
+
+    formCategory!.contents = [...existingBlocks, { kind: 'block', type: blockName }];
+
+    // 기존 툴박스 갱신
+    workspace.updateToolbox(toolboxConfig);
+
+    alert(`새 블록 "${blockName}"이(가) "폼" 카테고리에 성공적으로 추가되었습니다.`);
+  };
+
+  const generateHtmlCode = () => {
+    if (!workspace) {
+      return;
+    }
+    const code = htmlCodeGenerator.workspaceToCode(workspace);
+    setHtmlCode(code);
+  };
 
   return (
     <div className="flex flex-1">
