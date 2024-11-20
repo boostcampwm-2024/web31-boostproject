502c62dedf137117421cb7150aecb7c59723702c
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-20 17:17:25 +0900

    ✨ feat: debounce 메소드 구현

---
+++ apps/client/src/shared/utils/debounce.ts
@@ -0,0 +1,13 @@
+export const debounce = <T extends (...args: any[]) => any>(fn: T, delay: number) => {
+  let timeout: ReturnType<typeof setTimeout>;
+  return (...args: Parameters<T>) => {
+    let result: any;
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      result = fn(...args);
+    }, delay);
+    return result;
+  };
+};
