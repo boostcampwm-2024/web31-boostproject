eea292edab7dd5c2c5e7ab2f6b0e6e9e8297f73d
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-20 21:59:34 +0900

    🙀 chore: useRef를 사용하는 부분 삭제

--- apps/client/src/entities/workspace/CssTooltip.tsx
+++ apps/client/src/entities/workspace/CssTooltip.tsx
@@ -1,5 +1,4 @@
 import { createPortal } from 'react-dom';
-import { forwardRef } from 'react';
 
 type CssTooltipProps = {
   description: string;
@@ -8,21 +7,17 @@ type CssTooltipProps = {
   topY: number;
 };
 
-export const CssTooltip = forwardRef<HTMLDivElement, CssTooltipProps>(
-  ({ description, isOpen, leftX, topY }, ref) => {
-    if (!isOpen) {
-      return null;
-    }
-
-    return createPortal(
-      <div
-        className={`text-gray-white fixed left-0 top-0 rounded-3xl ${topY >= 0 ? 'rounded-tl-none' : 'rounded-bl-none'} bg-green-500 px-3 py-2`}
-        style={{ left: `${leftX + 16}px`, top: topY >= 0 ? `${topY + 4}px` : `${-topY}px` }}
-        ref={ref}
-      >
-        <p>{description}</p>
-      </div>,
-      document.body
-    );
+export const CssTooltip = ({ description, isOpen, leftX, topY }: CssTooltipProps) => {
+  if (!isOpen) {
+    return null;
   }
-);
+  return createPortal(
+    <div
+      className={`${isOpen ? '' : 'invisible'} text-gray-white fixed left-0 top-0 rounded-3xl ${topY >= 0 ? 'rounded-tl-none' : 'rounded-bl-none'} bg-green-500 px-3 py-2`}
+      style={{ left: `${leftX + 18}px`, top: topY >= 0 ? `${topY + 8}px` : `${-topY}px` }}
+    >
+      <p>{description}</p>
+    </div>,
+    document.body
+  );
+};
