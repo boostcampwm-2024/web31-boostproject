c942edb0b2dd241e37174af5d03a166729c4e2b4
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-21 02:29:41 +0900

    ✨ feat: 워크스페이스 페이지 렌더링 시 전역 상태 초기화 진행

--- apps/client/src/pages/WorkspacePage.tsx
+++ apps/client/src/pages/WorkspacePage.tsx
@@ -2,13 +2,19 @@ import { WorkspaceContent, WorkspacePageHeader } from '@/widgets';
 
 import { Loading } from '@/shared/ui';
 import { NotFound } from '@/pages/NotFound';
+import { useCssPropsStore } from '@/shared/store';
+import { useEffect } from 'react';
 import { useGetWorkspace } from '@/shared/hooks';
 import { useParams } from 'react-router-dom';
 
 export const WorkspacePage = () => {
   const { workspaceId } = useParams();
-
+  const { resetCssPropsStore } = useCssPropsStore();
   const { isPending, isError } = useGetWorkspace(workspaceId as string);
+
+  useEffect(() => {
+    resetCssPropsStore();
+  }, []);
 
   if (isError) {
     return <NotFound />;
