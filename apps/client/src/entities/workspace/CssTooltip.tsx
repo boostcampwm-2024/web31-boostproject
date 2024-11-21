2574f293b6fa721736c3586f729870c2fa96dcfe
leeyeongjae <xingxing2001@cau.ac.kr>
2024-11-21 11:21:29 +0900

    🙀 chore: 불필요한 코드 삭제

--- apps/client/src/entities/workspace/CssTooltip.tsx
+++ apps/client/src/entities/workspace/CssTooltip.tsx
@@ -13,7 +13,7 @@ export const CssTooltip = ({ description, isOpen, leftX, topY }: CssTooltipProps
   }
   return createPortal(
     <div
-      className={`${isOpen ? '' : 'invisible'} text-gray-white fixed left-0 top-0 rounded-3xl ${topY >= 0 ? 'rounded-tl-none' : 'rounded-bl-none'} bg-green-500 px-3 py-2`}
+      className={`text-gray-white fixed left-0 top-0 rounded-3xl ${topY >= 0 ? 'rounded-tl-none' : 'rounded-bl-none'} bg-green-500 px-3 py-2`}
       style={{ left: `${leftX + 18}px`, top: topY >= 0 ? `${topY + 8}px` : `${-topY}px` }}
     >
       <p>{description}</p>
