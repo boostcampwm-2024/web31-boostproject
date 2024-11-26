d1d67283a8bd51f8f905738409a46485fd100739
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-26 17:17:10 +0900

    ✨ feat: useBlocker를 통해 뒤로가기, Link 태그로 페이지를 이동하는 이벤트를 막는 기능 추가

--- apps/client/src/shared/hooks/usePreventLeaveWorkspacePage.ts
+++ apps/client/src/shared/hooks/usePreventLeaveWorkspacePage.ts
@@ -1,90 +1,46 @@
-import { useModalStore, useWorkspaceChangeStatusStore } from '@/shared/store';
-
+import { useBlocker } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useWorkspaceChangeStatusStore } from '@/shared/store';
 
 export const usePreventLeaveWorkspacePage = () => {
   const { isBlockChanged, isCssChanged } = useWorkspaceChangeStatusStore();
-  const {
-    isModalOpen,
-    openModal,
-    closeModal,
-    setModalContent,
-    setHandleModalCloseButton,
-    setHandleModalConfirmButton,
-  } = useModalStore();
+  let blocker = useBlocker(
+    ({ currentLocation, nextLocation }) =>
+      currentLocation.pathname !== nextLocation.pathname && (isBlockChanged || isCssChanged)
+  );
 
-  const navigate = useNavigate();
+  const promptOfLeavePage = `저장하지 않은 변경사항이 있습니다. 정말로 떠나시겠습니까?`;
+  useEffect(() => {
+    if (blocker.state === 'blocked') {
+      const confirmLeave = window.confirm(promptOfLeavePage);
+      if (confirmLeave) {
+        blocker.proceed();
+      } else {
+        blocker.reset();
+      }
+    }
+  }, [blocker.state, isBlockChanged, isCssChanged]);
 
   const handleBeforeUnload = (e: Event) => {
     e.preventDefault();
   };
 
-  const promptOfLeavePage = `저장하지 않은 변경사항이 있습니다.
-  정말로 떠나시겠습니까?`;
-
-  const handlePopState = (e: Event) => {
-    history.pushState(null, '', location.pathname);
-    // 이미 스택에서 한칸 바찜
-    console.log(history);
-    console.log(history);
-
-    if (isModalOpen) {
-      return;
-    }
-    if (isBlockChanged || isCssChanged) {
-      setModalContent(promptOfLeavePage);
-      setHandleModalCloseButton(() => {
-        history.pushState(null, '', location.pathname);
-        closeModal();
-      });
-      setHandleModalConfirmButton(() => {
-        navigate(-1);
-        closeModal();
-      });
-      openModal();
-    }
-  };
-
-  const handleClickLogo = () => {
-    if (isBlockChanged || isCssChanged) {
-      setModalContent(promptOfLeavePage);
-      setHandleModalCloseButton(() => {
-        closeModal();
-      });
-      setHandleModalConfirmButton(() => {
-        // navigate('/');
-        closeModal();
-      });
-      openModal();
-      return;
-    }
-    // navigate('/');
-  };
-
   const onPreventLeave = () => {
     window.addEventListener('beforeunload', handleBeforeUnload);
-    history.pushState(null, '', location.pathname);
-    window.addEventListener('popstate', handlePopState);
   };
 
   const offPreventLeave = () => {
     window.removeEventListener('beforeunload', handleBeforeUnload);
-    window.removeEventListener('popstate', handlePopState);
   };
 
   useEffect(() => {
     if (isBlockChanged || isCssChanged) {
-      console.log(`이벤트 등록`);
       onPreventLeave();
     } else {
-      console.log(`이벤트 제거`);
       offPreventLeave();
     }
     return () => {
       offPreventLeave();
     };
   }, [isBlockChanged, isCssChanged]);
-
-  return { handleClickLogo };
 };
