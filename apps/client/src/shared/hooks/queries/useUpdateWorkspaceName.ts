a75e8bf8da6ded6f67aff3342854718547d956ca
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-27 00:03:42 +0900

    🙀 chore: 변경된 타입 적용

--- apps/client/src/shared/hooks/queries/useUpdateWorkspaceName.ts
+++ apps/client/src/shared/hooks/queries/useUpdateWorkspaceName.ts
@@ -17,7 +17,12 @@ export const useUpdateWorkspaceName = () => {
     onSuccess: (data) => {
       toast.success('워크스페이스 이름이 변경되었습니다.');
       queryClient.setQueryData(workspaceKeys.detail(data.workspace_id), {
-        workspaceDto: { name: data.name, workspace_id: data.workspace_id },
+        workspaceDto: {
+          name: data.name,
+          workspaceId: data.workspace_id,
+          isCssRest: data.isCssReset,
+          totalCssPropertyObj: data.totalTotalCssPropertyObj,
+        },
       });
       queryClient.invalidateQueries({ queryKey: workspaceKeys.list() });
     },
