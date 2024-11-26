0cf1a7a1483476d60079d834ef8ed67ebd27e9dd
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-26 17:31:58 +0900

    ✨ feat: 워크스페이스 컬렉션 스키마 재정의 (CSS 관련 정보 추가)

--- apps/server/src/models/workspaceModel.ts
+++ apps/server/src/models/workspaceModel.ts
@@ -1,9 +1,22 @@
 import mongoose from 'mongoose';
 
+const optionScheme = new mongoose.Schema({
+  property: { type: String, required: true },
+  value: { type: String, required: true },
+  is_checked: { type: Boolean, required: true },
+});
+
+const cssListScheme = new mongoose.Schema({
+  class_name: { type: String, required: true },
+  option_list: [optionScheme],
+});
+
 const workspaceScheme = new mongoose.Schema({
   workspace_id: { type: String, required: true, unique: true },
   user_id: { type: String, required: true },
   name: { type: String, required: true, default: '워크스페이스 이름' },
+  css_list: [cssListScheme],
+  is_css_reset: { type: Boolean, required: true, default: false },
   thumbnail: { type: String },
   updated_at: { type: Date, required: true, default: Date.now },
 });
