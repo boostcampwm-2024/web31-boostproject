c49fbc1381444a4a74d3c889381fff0a6bfc4b8c
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-27 00:04:23 +0900

    ✨ feat: 워크스페이스 저장 버튼 저장 기능 구현

--- apps/client/src/entities/workspace/SaveButton.tsx
+++ apps/client/src/entities/workspace/SaveButton.tsx
@@ -1,11 +1,28 @@
-// TODO: 저장 로직 추가
+import { Spinner } from '@/shared/ui';
+import { useCssPropsStore } from '@/shared/store';
+import { useParams } from 'react-router-dom';
+import { useSaveWorkspace } from '@/shared/hooks';
+
+// TODO: 블록 상태 저장 로직 추가
 export const SaveButton = () => {
+  const { mutate: saveWorkspace, isPending } = useSaveWorkspace();
+  const { totalCssPropertyObj } = useCssPropsStore();
+  const workspaceId = useParams().workspaceId as string;
+  const handleClick = () => {
+    saveWorkspace({ workspaceId, totalCssPropertyObj });
+  };
+
   return (
     <button
-      onClick={() => {}}
+      onClick={handleClick}
       className="text-bold-rg w-16 rounded-[30px] bg-green-500 py-2 text-green-100 hover:border hover:border-green-500 hover:bg-green-100 hover:text-green-500"
+      disabled={isPending}
     >
-      저장
+      {isPending ? (
+        <Spinner width={4} height={4} backgroundColor="gray200" foregroundColor="grayWhite" />
+      ) : (
+        <p>저장</p>
+      )}
     </button>
   );
 };
