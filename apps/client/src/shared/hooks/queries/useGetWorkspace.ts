c96e58dbffa3274582c7316d2137d8c8ff9f403e
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-27 00:03:16 +0900

    ✨ feat: data fetching 성공 시 전역상태 cssProps, classBlockList 초기화

--- apps/client/src/shared/hooks/queries/useGetWorkspace.ts
+++ apps/client/src/shared/hooks/queries/useGetWorkspace.ts
@@ -1,3 +1,5 @@
+import { useClassBlockStore, useCssPropsStore } from '@/shared/store';
+
 import { WorkspaceApi } from '@/shared/api';
 import { getUserId } from '@/shared/utils';
 import toast from 'react-hot-toast';
@@ -5,9 +7,12 @@ import { useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { workspaceKeys } from '@/shared/hooks';
 
+// TODO : css reset 여부, 블록 상태도 초기화하기
 export const useGetWorkspace = (workspaceId: string) => {
   const workspaceApi = WorkspaceApi();
   const userId = getUserId();
+  const { initCssPropertyObj } = useCssPropsStore();
+  const { initClassBlockList } = useClassBlockStore();
   const { data, isPending, isError } = useQuery({
     queryKey: workspaceKeys.detail(workspaceId),
     queryFn: () => {
@@ -20,7 +25,10 @@ export const useGetWorkspace = (workspaceId: string) => {
       toast.error('워크스페이스 정보 불러오기 실패');
       return;
     }
-  }, [isError]);
-
+    if (data) {
+      initCssPropertyObj(data.workspaceDto.totalCssPropertyObj);
+      initClassBlockList(Object.keys(data.workspaceDto.totalCssPropertyObj));
+    }
+  }, [isError, data]);
   return { data, isPending, isError };
 };
