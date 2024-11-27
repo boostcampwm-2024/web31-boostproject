21cf7ba185e3fbec84c5d9f389a25698d86cf83b
inhachoi <80045891+inhachoi@users.noreply.github.com>
2024-11-27 17:15:31 +0900

    ☔️ test: loading 컴포넌트 스토리북 테스트

---
+++ apps/client/src/shared/ui/loading/Spinner.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Spinner } from './Spinner';
+
+const meta: Meta<typeof Spinner> = {
+  title: 'shared/ui/loading/Spinner',
+  component: Spinner,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Spinner>;
+
+export const Default: Story = {};
