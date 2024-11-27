b1c95c145adf5571d2d0565819b173c6ecd119a7
lee0jae330 <lee0jae330@users.noreply.github.com>
2024-11-27 08:49:53 +0000

    deploy: aa7e7b54fd997fff3e791f8b5375b2907665df70

---
+++ assets/GuideVideo.stories-DMoQG-Jr.js
@@ -0,0 +1,5 @@
+import{G as a}from"./GuideVideo-C9RQVDlF.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const i={title:"entities/home/GuideVideo",component:a,parameters:{layout:"centered"},tags:["auTODO:cs"],argTypes:{videoId:{control:"text",description:"유투브 영상 ID",defaultValue:"dQw4w9WgXcQ"}}},e={args:{videoId:"dQw4w9WgXcQ"}};var t,o,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    videoId: 'dQw4w9WgXcQ'
+  }
+}`,...(r=(o=e.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const n=["Default"];export{e as Default,n as __namedExportsOrder,i as default};
