9246d92fd633a39d6ff2ca30cec235cf402a701e
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-28 01:50:48 +0900

    💬 comment: 주석 삭제

--- apps/client/src/shared/hooks/queries/useSaveWorkspace.ts
+++ apps/client/src/shared/hooks/queries/useSaveWorkspace.ts
@@ -1,14 +1,13 @@
 import { TCanvas, TTotalCssPropertyObj } from '@/shared/types/workspaceType';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
+import { TBlock } from '@/shared/types';
 import { WorkspaceApi } from '@/shared/api';
 import { getUserId } from '@/shared/utils';
 import toast from 'react-hot-toast';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useWorkspaceChangeStatusStore } from '@/shared/store';
 import { workspaceKeys } from '../query-key/workspaceKeys';
-import { TBlock } from '@/shared/types';
 
-// TODO : 블록 상태도 저장해야함 Promise.all로 처리할 예정
 export const useSaveWorkspace = (workspaceId: string) => {
   const workspaceApi = WorkspaceApi();
   const userId = getUserId();
