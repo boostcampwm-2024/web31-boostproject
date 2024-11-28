4e05107a288f949868c91e32b501fd68f491365e
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-28 17:36:41 +0900

    🙀 chore: CssCategoryButton Props 변경에 따른 코드 수정

--- apps/client/src/widgets/workspace/css/CssCategoryBar.tsx
+++ apps/client/src/widgets/workspace/css/CssCategoryBar.tsx
@@ -5,7 +5,7 @@ export const CssCategoryBar = () => {
   return (
     <nav className="flex flex-shrink-0 flex-col gap-1.5 border-r border-r-gray-100 px-4 py-3">
       {cssCategoryList.map((cssCategory) => (
-        <CssCategoryButton cssCategory={cssCategory} key={cssCategory.category} />
+        <CssCategoryButton cssCategory={cssCategory.category} key={cssCategory.category} />
       ))}
     </nav>
   );
