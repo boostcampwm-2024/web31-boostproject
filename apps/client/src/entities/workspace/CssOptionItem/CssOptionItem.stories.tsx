a9976ee250fd8bb62e95a2d15b00c78b17100c88
lee0jae330 <xingxing2001@cau.ac.kr>
2024-11-29 04:40:26 +0900

    ✨ feat: CssOptionItem 스토리북 제작

--- apps/client/src/entities/workspace/CssOptionItem/CssOptionItem.stories.tsx
+++ apps/client/src/entities/workspace/CssOptionItem/CssOptionItem.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react';
 
 import { CssOptionItem } from './CssOptionItem';
+import { cssCategoryList } from '@/shared/utils';
 
 const meta: Meta<typeof CssOptionItem> = {
   title: 'entities/workspace/CssOptionItem',
@@ -17,6 +18,34 @@ type Story = StoryObj<typeof CssOptionItem>;
 
 export const Default: Story = {
   args: {
-    // propsname: value,
+    cssItem: cssCategoryList[0].items[0],
+    index: 0,
+  },
+};
+
+export const Resize: Story = {
+  render: () => {
+    return (
+      <div style={{ width: '400px', resize: 'horizontal', overflow: 'auto' }}>
+        <CssOptionItem cssItem={cssCategoryList[0].items[0]} index={0} />
+      </div>
+    );
+  },
+};
+
+export const ResizeMultipleItems: Story = {
+  render: () => {
+    const selectedCssCategory = '레이아웃';
+    return (
+      <div style={{ width: '400px', resize: 'horizontal', overflow: 'auto' }}>
+        {cssCategoryList
+          .filter((cssCategory) => cssCategory.category === selectedCssCategory)
+          .map((cssCategory) =>
+            cssCategory.items.map((cssItem, index) => (
+              <CssOptionItem cssItem={cssItem} index={index} key={cssItem.label} />
+            ))
+          )}
+      </div>
+    );
   },
 };
