3e2b87a80511d85f3a3bdda8817332735ddb5f9a
lee0jae330 <lee0jae330@users.noreply.github.com>
2024-11-29 01:26:30 +0000

    deploy: f6854ef3c4b0ec72f2346a1f6b5dfe9bc799b2c0

---
+++ assets/WorkspaceList.stories-pvsf3gSL.js
@@ -0,0 +1,39 @@
+import{j as s}from"./jsx-runtime-DR9Q75dM.js";import{W as u}from"./WorkspaceList-B56Rs5FV.js";import{g as c,v as d}from"./HoveredEmptyWorkspace-CE17M2DX.js";import"./Spinner-QuWSqHI9.js";import"./index-DRjF_FHU.js";import"./GuideVideo-MGmf4GR2.js";import"./WorkspaceLoadError-Cdw2mO9Z.js";import"./NotHoveredEmptyWorkspace-B7Wda7nZ.js";import"./RedoButton-C_Lrkfv2.js";import"./CircleButton-CZ-LhlOb.js";import"./SquareButton-hj7tViqk.js";import"./Logo-BptDqOCu.js";import"./index-x70cJmE6.js";import"./index-BXCeiWJk.js";import"./ToasterWithMax-D4jPvwZi.js";import"./index-D1AAVYfs.js";import"./Loading-CVQUyNmW.js";import"./SkeletonWorkspace-D4UalYBK.js";import"./SkeletonWorkspaceList-LqBByOXs.js";import"./Select-BczipgGO.js";import"./useWorkspaceStore-CrNgioXB.js";import"./UndoButton-CG9EXSv1.js";import"./CssCategoryButton-kLrWYMB7.js";import"./client-Dbo3a7kb.js";import"./QueryClientProvider-BdRu95_o.js";import"./plus-DWHtmhlY.js";import"./question-CX5dqjCQ.js";const q={title:"widgets/home/WorkspaceList",component:u,parameters:{layout:"fullscreen"},decorators:[r=>s.jsx("div",{className:"p-4",children:s.jsx(r,{})})],tags:["autodocs"]},e={args:{workspaceList:[{name:"Workspace 1",updated_at:new Date().toISOString(),user_id:c(),workspace_id:d(),isCssReset:!1,thumbnail:"",totalTotalCssPropertyObj:{example:{checkedCssPropertyObj:{},cssOptionObj:{}}}}]}},t={args:{workspaceList:Array.from({length:4}).map((r,l)=>({name:`Workspace ${l}`,updated_at:new Date().toISOString(),user_id:c(),workspace_id:d(),isCssReset:!1,thumbnail:"",totalTotalCssPropertyObj:{example:{checkedCssPropertyObj:{},cssOptionObj:{}}}}))}};var o,a,p;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    workspaceList: [{
+      name: 'Workspace 1',
+      updated_at: new Date().toISOString(),
+      user_id: getUserId(),
+      workspace_id: v4(),
+      isCssReset: false,
+      thumbnail: '',
+      totalTotalCssPropertyObj: {
+        example: {
+          checkedCssPropertyObj: {},
+          cssOptionObj: {}
+        }
+      }
+    }]
+  }
+}`,...(p=(a=e.parameters)==null?void 0:a.docs)==null?void 0:p.source}}};var n,i,m;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    workspaceList: Array.from({
+      length: 4
+    }).map((_, idx) => {
+      return {
+        name: \`Workspace \${idx}\`,
+        updated_at: new Date().toISOString(),
+        user_id: getUserId(),
+        workspace_id: v4(),
+        isCssReset: false,
+        thumbnail: '',
+        totalTotalCssPropertyObj: {
+          example: {
+            checkedCssPropertyObj: {},
+            cssOptionObj: {}
+          }
+        }
+      };
+    })
+  }
+}`,...(m=(i=t.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const z=["Default","MultipleItems"];export{e as Default,t as MultipleItems,z as __namedExportsOrder,q as default};
