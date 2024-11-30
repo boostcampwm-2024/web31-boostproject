9c0b63e2798c747df63c425a604adbd6dcfdbe81
chichoc <kny6152@naver.com>
2024-11-30 17:04:39 +0900

    💄 design: 텍스트길이와 상관없이 가운데 정렬

--- apps/client/src/shared/ui/error/ErrorContent.tsx
+++ apps/client/src/shared/ui/error/ErrorContent.tsx
@@ -8,7 +8,7 @@ export const ErrorContent = ({ description }: ErrorContentProps) => {
   return (
     <>
       <HomeHeader isBlack={false} />
-      <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col gap-6">
+      <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center gap-6">
         <img src="/images/not_found.png" width={160} height={160} alt="not_found" />
         <p className="text-medium-md whitespace-pre-line text-center text-gray-200">
           {description}
