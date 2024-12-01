a70d4b4528c7b0e5a2500098d5543fe149a28a0b
Yujin <yujinyll9148@gmail.com>
2024-12-02 06:12:17 +0900

    🐛 fix: 플 복제만들 때는 이미지도 s3에 올려서 url 새로 만들기

--- apps/server/src/services/workspaceService.ts
+++ apps/server/src/services/workspaceService.ts
@@ -26,22 +26,31 @@ export const WorkspaceService = () => {
 
     const newWorkspaceId = crypto.randomUUID();
 
-    const clonedWorkspaceData = {
-      workspace_id: newWorkspaceId,
-      user_id: userId,
-      name: sampleWorkspace.name,
-      canvas: sampleWorkspace.canvas,
-      css_list: sampleWorkspace.css_list,
-      class_block_list: sampleWorkspace.class_block_list,
-      is_css_reset: sampleWorkspace.is_css_reset,
-      thumbnail: sampleWorkspace.thumbnail,
-      updated_at: new Date(),
-    };
+    try {
+      const sampleThumbnailResponse = await fetch(sampleWorkspace.thumbnail!);
+      const imageBuffer = Buffer.from(await sampleThumbnailResponse.arrayBuffer());
+      const thumbnailUrl = await uploadThumbnailImage(userId, newWorkspaceId, imageBuffer);
 
-    const clonedWorkspace = new Workspace(clonedWorkspaceData);
-    await clonedWorkspace.save();
+      const clonedWorkspaceData = {
+        workspace_id: newWorkspaceId,
+        user_id: userId,
+        name: sampleWorkspace.name,
+        canvas: sampleWorkspace.canvas,
+        css_list: sampleWorkspace.css_list,
+        class_block_list: sampleWorkspace.class_block_list,
+        is_css_reset: sampleWorkspace.is_css_reset,
+        thumbnail: thumbnailUrl,
+        updated_at: new Date(),
+      };
 
-    return newWorkspaceId;
+      const clonedWorkspace = new Workspace(clonedWorkspaceData);
+      await clonedWorkspace.save();
+
+      return newWorkspaceId;
+    } catch (error) {
+      console.error('Error while cloning workspace:', error);
+      throw new Error('워크스페이스 복제 중 오류가 발생했습니다.');
+    }
   };
 
   const findWorkspaceListByPage = async (
@@ -194,6 +203,42 @@ export const WorkspaceService = () => {
     } finally {
       session.endSession();
     }
+  };
+
+  const uploadThumbnailImage = async (
+    userId: string,
+    workspaceId: string,
+    imageBuffer: Buffer
+  ): Promise<string> => {
+    // 이미지 리사이징 및 webp 변환
+    const webpThumbnail = await sharp(imageBuffer)
+      .resize({
+        width: 528,
+        height: 360,
+        fit: 'inside',
+        background: { r: 255, g: 255, b: 255, alpha: 1 },
+      })
+      .webp()
+      .toBuffer();
+
+    // S3 업로드
+    const upload = new Upload({
+      client: S3,
+      params: {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: `thumbnail/${userId}/${workspaceId}.webp`,
+        ACL: 'public-read',
+        Body: webpThumbnail,
+        ContentType: 'image/webp',
+      },
+    });
+
+    const uploadResult = await upload.done();
+    if (!uploadResult) {
+      throw new Error('Failed to upload thumbnail');
+    }
+
+    return uploadResult.Location as string;
   };
 
   return {
