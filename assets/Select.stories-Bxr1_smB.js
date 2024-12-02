0421d33e952260ce1b21b2c733f78cc51ac5369b
Ujaa <Ujaa@users.noreply.github.com>
2024-12-02 06:37:53 +0000

    deploy: 2c6aea91a568b17d2707c60cc28e6438cfd58034

---
+++ assets/Select.stories-Bxr1_smB.js
@@ -0,0 +1,17 @@
+import{S as t}from"./Select-BMkjBj55.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";import"./index-BXCeiWJk.js";const i={title:"shared/ui/Select/Select",component:t,parameters:{layout:"centered"},tags:["autodocs"]},e={args:{options:[{value:"1",label:"Option 1"},{value:"2",label:"Option 2"},{value:"3",label:"Option 3"}],value:"",placeholder:"Select an option",onChange:l=>console.log(l)}};var a,o,n;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    options: [{
+      value: '1',
+      label: 'Option 1'
+    }, {
+      value: '2',
+      label: 'Option 2'
+    }, {
+      value: '3',
+      label: 'Option 3'
+    }],
+    value: '',
+    placeholder: 'Select an option',
+    onChange: (value: string) => console.log(value)
+  }
+}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const u=["Default"];export{e as Default,u as __namedExportsOrder,i as default};
