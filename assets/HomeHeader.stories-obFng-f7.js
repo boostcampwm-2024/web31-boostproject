a35a4b350cace871e503db42d6efb7c59e3e6e40
lee0jae330 <lee0jae330@users.noreply.github.com>
2024-12-03 03:05:13 +0000

    deploy: f3b03150818d6a01127d98f1ec4d17ce61af454d

--- assets/HomeHeader.stories-obFng-f7.js
+++
@@ -1,9 +0,0 @@
-import{H as i}from"./HomeHeader-CSIv8bYU.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";import"./plus-qIVVYTMO.js";import"./SquareButton-hj7tViqk.js";import"./Logo-Ca0csvGK.js";import"./index-x70cJmE6.js";import"./index-BXCeiWJk.js";import"./ToasterWithMax-D4jPvwZi.js";import"./index-D1AAVYfs.js";import"./Loading-CVQUyNmW.js";import"./Spinner-CUCGrwDX.js";import"./useWorkspaceStore-BD1duu30.js";import"./SkeletonWorkspace-D4UalYBK.js";import"./SkeletonWorkspaceList-LqBByOXs.js";import"./HoveredEmptyWorkspace-YXBn9bzz.js";import"./QueryClientProvider-BdRu95_o.js";import"./GuideVideo-MGmf4GR2.js";import"./WorkspaceLoadError-FmbY_INt.js";import"./NotHoveredEmptyWorkspace-CddEnm-X.js";import"./question-CX5dqjCQ.js";import"./CssCategoryButton-BHr79OL3.js";import"./client-Dbo3a7kb.js";const z={title:"widgets/home/HomeHeader",component:i,parameters:{layout:"fullscreen"},tags:["autodocs"]},r={args:{}},o={args:{isBlack:!0}};var t,e,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    // propsname: value,
-  }
-}`,...(a=(e=r.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};var m,p,s;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
-  args: {
-    isBlack: true
-  }
-}`,...(s=(p=o.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};const A=["Default","Dark"];export{o as Dark,r as Default,A as __namedExportsOrder,z as default};
