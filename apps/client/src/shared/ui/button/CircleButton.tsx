e9ba2435816e0db8b5a3e699bdfaade5de44d8fe
chichoc <kny6152@naver.com>
2024-12-03 17:40:23 +0900

    💄 design: CircleButton outline스타일 추가 & 클래스 변수로 구분

--- apps/client/src/shared/ui/button/CircleButton.tsx
+++ apps/client/src/shared/ui/button/CircleButton.tsx
@@ -7,6 +7,7 @@ type CircleButtonProps = {
   height?: string;
   onClick: () => void;
   disable?: boolean;
+  variant?: 'filled' | 'outlined';
 };
 
 /**
@@ -21,12 +22,18 @@ export const CircleButton = ({
   height,
   onClick,
   disable = false,
+  variant = 'filled',
 }: CircleButtonProps) => {
+  const baseClasses = `flex items-center justify-center rounded-full disabled:cursor-not-allowed ${width} ${height}`;
+  const filledClasses = `bg-green-500 text-green-100 hover:border hover:border-green-500 hover:bg-green-100 hover:text-green-500 disabled:border-green-300 disabled:bg-green-300`;
+  const outlinedClasses = `border border-gray-100 text-gray-300 hover:text-gray-500 hover:border-gray-300`;
+
+  const variantClasses = variant === 'filled' ? filledClasses : outlinedClasses;
+
   return (
     <button
       onClick={onClick}
-      className={`flex items-center ${className} ${width} ${height} justify-center rounded-full bg-green-500 text-green-100 hover:border hover:border-green-500 hover:bg-green-100 hover:text-green-500 disabled:cursor-not-allowed disabled:border-green-300 disabled:bg-green-300`}
-      // TODO: disabled의 경우 hover 효과 없애거나 hover 효과 변경 (논의 필요)
+      className={`${baseClasses} ${variantClasses} ${className}`}
       disabled={disable}
     >
       {children}
