ba3ffced5b240b401efc0cf3a4f423605fd2903f
chichoc <kny6152@naver.com>
2024-12-03 17:42:27 +0900

    ✨ feat: 단계별 텍스트 중 주요 단어 강조

--- apps/client/src/widgets/workspace/CoachMark/CoachMark.tsx
+++ apps/client/src/widgets/workspace/CoachMark/CoachMark.tsx
@@ -7,25 +7,51 @@ import TabbedToolbox from '@/core/tabbedToolbox';
 const steps = [
   {
     title: 'HTML 태그 블록 조립하기',
-    content: '오른쪽 HTML 태그 탭에서 블록을 가져와 작업 공간에서 조립할 수 있어요',
+    content: (
+      <>
+        오른쪽 <span className="coachMarkHighlightText">HTML 태그 탭</span>에서 블록을 가져와 작업
+        공간에서 조립할 수 있어요
+      </>
+    ),
   },
   {
     title: 'CSS 클래스 블록 생성 후 조립하기',
-    content:
-      '원하는 CSS 클래스 블록을 생성할 수 있어요. \n 생성된 블록은 HTML 블록에 조립할 수 있어요',
+    content: (
+      <>
+        원하는 <span className="coachMarkHighlightText">CSS 클래스 블록</span>을 생성할 수 있어요.{' '}
+        <br />
+        생성된 블록은 HTML 블록에 조립할 수 있어요
+      </>
+    ),
   },
   {
     title: '스타일 속성 추가하기',
-    content: '생성한 CSS 클래스 블록을 선택해 원하는 스타일 속성을 추가할 수 있어요',
+    content: (
+      <>
+        생성한 <span className="coachMarkHighlightText">CSS 클래스 블록</span>을 선택해 원하는{' '}
+        <span className="coachMarkHighlightText">스타일 속성</span>을 추가할 수 있어요
+      </>
+    ),
   },
   {
     title: '미리보기와 코드 확인하기',
-    content:
-      '미리보기 탭에서는 블록 코딩으로 만든 화면을,\n HTML/CSS 탭에서는 코드를 확인할 수 있어요.',
+    content: (
+      <>
+        <span className="coachMarkHighlightText">미리보기</span> 탭에서는 블록 코딩으로 만든 화면을,{' '}
+        <br />
+        <span className="coachMarkHighlightText">HTML/CSS</span> 탭에서는 코드를 확인할 수 있어요.
+      </>
+    ),
   },
   {
     title: '저장하고 불러오기',
-    content: '저장하지 않고 나가면 블록이 사라져요. 변경 사항은 되돌리거나 다시 적용할 수 있어요.',
+    content: (
+      <>
+        <span className="coachMarkHighlightText">저장</span>하지 않고 나가면 블록이 사라져요. <br />
+        변경 사항은 <span className="coachMarkHighlightText">되돌리거나 다시 적용</span>할 수
+        있어요.
+      </>
+    ),
   },
 ];
 
@@ -82,6 +108,7 @@ export const CoachMark = () => {
               onClick={closeCoachMark}
               disable={currentStep === stepsLength}
               className="text-bold-sm h-8 w-16"
+              variant="outlined"
             >
               그만 보기
             </CircleButton>
