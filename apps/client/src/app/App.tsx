10c280ad502fd3ad882d58c93e3931dd043f4340
inhachoi <chlruddlf73@naver.com>
2024-12-04 12:50:40 +0900

    🔨 refactor: 레이지 로딩 적용

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -1,25 +1,51 @@
-import { HomePage, NotFound, WorkspacePage } from '@/pages';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { ToasterWithMax } from '@/shared/ui';
 import { ErrorPage } from '@/pages/ErrorPage/ErrorPage';
+import { lazy, Suspense } from 'react';
+
+// lazy 로딩
+const HomePage = lazy(() =>
+  import('@/pages/HomePage/HomePage').then((module) => ({ default: module.HomePage }))
+);
+
+const WorkspacePage = lazy(() =>
+  import('@/pages/Workspacepage/WorkspacePage').then((module) => ({
+    default: module.WorkspacePage,
+  }))
+);
+
+const NotFound = lazy(() =>
+  import('@/pages/NotFound/NotFound').then((module) => ({ default: module.NotFound }))
+);
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />,
+    element: (
+      <Suspense fallback={<div>홈 페이지로 로딩중...</div>}>
+        <HomePage />
+      </Suspense>
+    ),
     errorElement: <ErrorPage />,
   },
   {
     path: '/workspace/:workspaceId',
-    element: <WorkspacePage />,
+    element: (
+      <Suspense fallback={<div>워크스페이스 페이지로 로딩중...</div>}>
+        <WorkspacePage />
+      </Suspense>
+    ),
     errorElement: <ErrorPage />,
   },
   {
     path: '*',
-    element: <NotFound />,
+    element: (
+      <Suspense fallback={<div>페이지를 찾을 수 없습니다...</div>}>
+        <NotFound />
+      </Suspense>
+    ),
   },
 ]);
-
 export const App = () => (
   <>
     <ToasterWithMax />
