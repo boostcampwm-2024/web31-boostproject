b9bb9fa9db01a6ce318ef82b2621069ad6082b65
lee0jae330 <lee0jae330@users.noreply.github.com>
2024-12-04 02:26:31 +0000

    deploy: e375d354db725865d55bc1d540a464db76c3c6fc

---
+++ assets/WorkspaceItem.stories-DnYtEtlY.js
@@ -0,0 +1,16 @@
+import{j as e}from"./jsx-runtime-DR9Q75dM.js";import{a as s}from"./HoveredEmptyWorkspace-VOvy3jB5.js";import{a as m}from"./chunk-D5ZWXAHU-CGElDDNX.js";import"./index-DRjF_FHU.js";import"./html2canvas.esm-T2p3Seyt.js";import"./QueryClientProvider-BdRu95_o.js";import"./index-D1AAVYfs.js";import"./GuideVideo-MGmf4GR2.js";import"./plus-dioM8lOZ.js";import"./SquareButton-hj7tViqk.js";import"./Logo-DlHd6_yt.js";import"./index-x70cJmE6.js";import"./index-BXCeiWJk.js";import"./ToasterWithMax-D4jPvwZi.js";import"./Loading-CVQUyNmW.js";import"./Spinner-BB_XZPSl.js";import"./SkeletonWorkspace-D4UalYBK.js";import"./SkeletonWorkspaceList-LqBByOXs.js";import"./WorkspaceLoadError-BuktD2D2.js";import"./NotHoveredEmptyWorkspace-loFxOJJf.js";import"./question-BUxmZXrP.js";import"./CssCategoryButton-BnEm8J6w.js";import"./client-Dbo3a7kb.js";import"./v4-CQkTLCs1.js";const O={title:"entities/home/WorkspaceItem",component:s,parameters:{layout:"fullscreen"},tags:["autodocs"]},t={args:{workspaceId:"4ddcbf25-acb0-42cd-8a97-aeda515e26db",title:"스토리북용 워크스페이스",thumbnail:"",lastEdited:"2024-11-28T09:34:45.106+00:00",onClick:()=>{m("workspaceItem clicked")()}},render:i=>e.jsx("div",{className:"flex items-center justify-center p-4",children:e.jsx("ul",{className:"w-[264px]",children:e.jsx(s,{...i})})})};var r,a,o;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    workspaceId: '4ddcbf25-acb0-42cd-8a97-aeda515e26db',
+    title: '스토리북용 워크스페이스',
+    thumbnail: '',
+    lastEdited: '2024-11-28T09:34:45.106+00:00',
+    onClick: () => {
+      action('workspaceItem clicked')();
+    }
+  },
+  render: args => <div className="flex items-center justify-center p-4">
+      <ul className="w-[264px]">
+        <WorkspaceItem {...args} />
+      </ul>
+    </div>
+}`,...(o=(a=t.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const R=["Default"];export{t as Default,R as __namedExportsOrder,O as default};
---
+++ assets/useCoachMarkStore-CAqI6O7S.js
@@ -0,0 +1,1 @@
+import{d as a}from"./html2canvas.esm-T2p3Seyt.js";const o=a()(r=>({isCoachMarkOpen:!0,currentStep:0,openCoachMark:()=>r({isCoachMarkOpen:!0,currentStep:0}),closeCoachMark:()=>r({isCoachMarkOpen:!1}),setCurrentStep:e=>r({currentStep:e})}));export{o as u};
