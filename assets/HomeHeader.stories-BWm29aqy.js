2d63b2292dd7911c7c06ce6bdb4a0d8dc6b9768b
inhachoi <inhachoi@users.noreply.github.com>
2024-12-04 08:46:53 +0000

    deploy: aae7749aedfc09cab0e037ceb9ee5d7360282865

--- assets/HomeHeader.stories-BWm29aqy.js
+++
@@ -1,9 +0,0 @@
-import{H as i}from"./HomeHeader-BJV8Ongq.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";import"./plus-C7enkl2d.js";import"./SquareButton-eIFPi1Hz.js";import"./Logo-DlHd6_yt.js";import"./index-x70cJmE6.js";import"./index-BXCeiWJk.js";import"./ToasterWithMax-D4jPvwZi.js";import"./index-D1AAVYfs.js";import"./Loading-CVQUyNmW.js";import"./Spinner-BB_XZPSl.js";import"./html2canvas.esm-T2p3Seyt.js";import"./SkeletonWorkspace-D4UalYBK.js";import"./SkeletonWorkspaceList-LqBByOXs.js";import"./HoveredEmptyWorkspace-B7d4ySV4.js";import"./QueryClientProvider-BdRu95_o.js";import"./GuideVideo-MGmf4GR2.js";import"./WorkspaceLoadError-BuktD2D2.js";import"./NotHoveredEmptyWorkspace-loFxOJJf.js";import"./question-BUxmZXrP.js";import"./CssCategoryButton-BCvsdXHF.js";import"./client-Dbo3a7kb.js";const z={title:"widgets/home/HomeHeader",component:i,parameters:{layout:"fullscreen"},tags:["autodocs"]},r={args:{}},o={args:{isBlack:!0}};var t,e,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    // propsname: value,
-  }
-}`,...(a=(e=r.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};var m,p,s;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
-  args: {
-    isBlack: true
-  }
-}`,...(s=(p=o.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};const A=["Default","Dark"];export{o as Dark,r as Default,A as __namedExportsOrder,z as default};
---
+++ assets/WorkspaceGrid-C2jJPMUx.js
@@ -0,0 +1,2 @@
+import{j as s}from"./jsx-runtime-DR9Q75dM.js";const i=({children:r})=>s.jsx("div",{children:s.jsx("div",{className:"grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4",children:r})});i.__docgenInfo={description:`@description
+워크스페이스 그리드 컴포넌트`,methods:[],displayName:"WorkspaceGrid"};export{i as W};
