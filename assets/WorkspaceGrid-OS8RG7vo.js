5ff731589f91d2adae478b479225e1178cb511d0
Ujaa <Ujaa@users.noreply.github.com>
2024-12-04 08:01:57 +0000

    deploy: fc165ec7e5ed26171749b5ef3cd2382dd3bd699d

--- assets/WorkspaceGrid-OS8RG7vo.js
+++
@@ -1,2 +0,0 @@
-import{j as s}from"./jsx-runtime-DR9Q75dM.js";const i=({children:r})=>s.jsx("ul",{className:"grid-cols-list grid w-[1128px] justify-start gap-x-6 gap-y-8",children:s.jsx("li",{children:r})});i.__docgenInfo={description:`@description
-워크스페이스 그리드 컴포넌트`,methods:[],displayName:"WorkspaceGrid"};export{i as W};
