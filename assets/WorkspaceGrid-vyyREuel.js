c5dd94da9d7fe0b710e3404ae370421ad002a884
Ujaa <Ujaa@users.noreply.github.com>
2024-12-04 02:35:01 +0000

    deploy: 81f8f295452b7759a604bffc3a4ee57989752d5c

--- assets/WorkspaceGrid-vyyREuel.js
+++
@@ -1,2 +0,0 @@
-import{j as r}from"./jsx-runtime-DR9Q75dM.js";const i=({children:s})=>r.jsx("ul",{className:"grid-cols-list grid w-[1128px] justify-start gap-x-6 gap-y-8",children:s});i.__docgenInfo={description:`@description
-워크스페이스 그리드 컴포넌트`,methods:[],displayName:"WorkspaceGrid"};export{i as W};
