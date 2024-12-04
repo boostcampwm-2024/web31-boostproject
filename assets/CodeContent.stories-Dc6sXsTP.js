695a908819f513c412f1f52bf1da175cc313706b
lee0jae330 <lee0jae330@users.noreply.github.com>
2024-12-04 17:36:48 +0000

    deploy: 6622944c5eab035d869976d1f202e72ab23ec445

---
+++ assets/CodeContent.stories-Dc6sXsTP.js
@@ -0,0 +1,25 @@
+import{C as n}from"./CodeContent-DoxBelqC.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";import"./CodeViewer.module-CZ8GGqUa.js";const r={title:"shared/code-highlighter/CodeContent",component:n,parameters:{layout:"centered"},tags:["autodocs"]},e={args:{code:`<html>
+    <head></head>
+    <body>
+      <div>
+        <h1>title</h1>
+        <p>content</p>
+      </div>
+    </body>
+  </html>`,codeLineList:["<html>","  <head></head>","  <body>","    <div>","      <h1>title</h1>","      <p>content</p>","    </div>","  </body>","</html>"],selectedBlockStartLine:5,selectedBlockLength:7,selectedBlockType:"BOOLOCK_SYSTEM_html"}};var t,o,d;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    code: \`<html>
+    <head></head>
+    <body>
+      <div>
+        <h1>title</h1>
+        <p>content</p>
+      </div>
+    </body>
+  </html>\`,
+    codeLineList: ['<html>', '  <head></head>', '  <body>', '    <div>', '      <h1>title</h1>', '      <p>content</p>', '    </div>', '  </body>', '</html>'],
+    selectedBlockStartLine: 5,
+    selectedBlockLength: 7,
+    selectedBlockType: 'BOOLOCK_SYSTEM_html'
+  }
+}`,...(d=(o=e.parameters)==null?void 0:o.docs)==null?void 0:d.source}}};const s=["Default"];export{e as Default,s as __namedExportsOrder,r as default};
---
+++ assets/LineNumber.stories-BZXD6iuP.js
@@ -0,0 +1,5 @@
+import{L as s}from"./LineNumbers-eZdDlBqZ.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";import"./CodeViewer.module-CZ8GGqUa.js";const m={title:"shared/code-highlighter/LineNumbers",component:s,parameters:{layout:"centered"},tags:["autodocs"]},e={args:{codeLineList:["line1","line2","line3"]}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    codeLineList: ['line1', 'line2', 'line3']
+  }
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const l=["Default"];export{e as Default,l as __namedExportsOrder,m as default};
