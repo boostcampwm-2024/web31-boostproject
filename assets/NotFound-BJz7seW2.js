0a0b9d2c912a865f9bb4c4268507fe8e160155df
lee0jae330 <lee0jae330@users.noreply.github.com>
2024-12-06 01:42:05 +0000

    deploy: c750deda9959058e52abddb09276dad1194da1f4

---
+++ assets/NotFound-BJz7seW2.js
@@ -0,0 +1,3 @@
+import{j as o}from"./jsx-runtime-DR9Q75dM.js";import{E as r}from"./ErrorContent-DWCu90p-.js";const t=()=>o.jsx(o.Fragment,{children:o.jsx(r,{description:`유효한 페이지가 아닙니다! 
+다른 페이지에서 만나요!`})});t.__docgenInfo={description:`@description
+404 페이지 컴포넌트`,methods:[],displayName:"NotFound"};export{t as N};
