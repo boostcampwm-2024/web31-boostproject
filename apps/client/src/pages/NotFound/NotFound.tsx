fc7e5f1f4fe5f42c4e9e3cf9d43bf83e77eef49b
leeyeongjae <xingxing2001@cau.ac.kr>
2024-12-06 10:37:02 +0900

    🚨 !HOTFIX!: meta 태그를 인식하지 못하는 문제 해결

--- apps/client/src/pages/NotFound/NotFound.tsx
+++ apps/client/src/pages/NotFound/NotFound.tsx
@@ -1,4 +1,3 @@
-import { Helmet } from 'react-helmet-async';
 import { ErrorContent } from '@/shared/ui/error/ErrorContent';
 
 // TODO: 메세지 상수화 shared/utils/constants.ts 안에 관리
@@ -10,13 +9,6 @@ import { ErrorContent } from '@/shared/ui/error/ErrorContent';
 export const NotFound = () => {
   return (
     <>
-      <Helmet>
-        <title>BooLock - 페이지를 찾을 수 없음</title>
-        <meta
-          name="description"
-          content="요청한 페이지를 찾을 수 없습니다. 다른 페이지를 확인해 주세요."
-        />
-      </Helmet>
       <ErrorContent description={`유효한 페이지가 아닙니다! \n다른 페이지에서 만나요!`} />
     </>
   );
